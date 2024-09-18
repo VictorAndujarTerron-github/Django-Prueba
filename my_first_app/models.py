3c5f350453e8d816273a7f39bf7496ec68b645ff
victor <victor.andujar@upc.edu>
2024-09-18 13:41:35 +0200

    commit antes de hacer pull por temas Readme

--- my_first_app/models.py
+++ my_first_app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Car(models.Model):
     title = models.TextField(max_length = 250)
     year = models.TextField(max_length = 4, null=True)
-    color = models.models.TextField(max_length = 25, null=True)
+    color = models.TextField(max_length = 25, null=True)
 
     # Este metodo imprime los datos que tenemos del objeto que llama a la función
     def __str__(self):
