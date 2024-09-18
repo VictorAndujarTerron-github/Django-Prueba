d45ab2874d82ddd5063aa02409cdb91277b8d847
victor <victor.andujar@upc.edu>
2024-09-18 12:12:29 +0200

    Atributo de color añadido a la clase Car y modificación de la función str para que imprima el color

--- my_first_app/models.py
+++ my_first_app/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class Car(models.Model):
     title = models.TextField(max_length = 250)
     year = models.TextField(max_length = 4, null=True)
+    color = models.models.TextField(max_length = 25, null=True)
 
     # Este metodo imprime los datos que tenemos del objeto que llama a la función
     def __str__(self):
-        return f"{self.title} - {self.year}"
+        return f"{self.title} - {self.year} - {self.color}"
