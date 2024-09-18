4c367b4460341ac2ec13c98d17ecb24ca9eddf1c
victor <victor.andujar@upc.edu>
2024-09-18 12:08:54 +0200

    Definición del metodo/función __str__ para imprimir los valores de un objeto de la clase Car

--- my_first_app/models.py
+++ my_first_app/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Car(models.Model):
     title = models.TextField(max_length = 250)
     year = models.TextField(max_length = 4, null=True)
+
+    # Este metodo imprime los datos que tenemos del objeto que llama a la función
+    def __str__(self):
+        return f"{self.title} - {self.year}"
