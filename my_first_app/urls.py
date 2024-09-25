1ec9eb7afeafd7f2b0eb0d88eafb3731d461eebc
victor <victor.andujar@upc.edu>
2024-09-25 08:00:04 +0200

    Todos los cambios realizados en la clase nº 14 del curso de Platzi

--- my_first_app/urls.py
+++ my_first_app/urls.py
@@ -16,14 +16,11 @@ Including another URLconf
 """
 from django.http import HttpResponse
 from django.urls import path
-
-def my_view(request, *args, **kwargs):
-    print(args)
-    print(kwargs)
-    return HttpResponse("")
+from .views import my_test_view, my_view, CarListView
 
 urlpatterns = [
-    path('listado/', my_view),
-    path('detalle/<int:id>', my_view),
-    path('marcas/<str:brand>', my_view)
+    # path('listado/', my_view),
+    path('listado/', CarListView.as_view()),
+    path('detalle/<int:id>', my_test_view),
+    path('marcas/<str:brand>', my_test_view),
 ]
--- my_first_app/views.py
+++ my_first_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from my_first_app.models import Car
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
 # Create your views here.
 def my_view(request):
@@ -8,3 +11,18 @@ def my_view(request):
         "car_list": car_list
     }
     return render(request, "my_first_app/car_list.html", context)
+
+def my_test_view(request, *args, **kwargs):
+    print(args)
+    print(kwargs)
+    return HttpResponse("")
+
+class CarListView(TemplateView):
+    template_name = "my_first_app/car_list.html"
+
+    def get_context_data(self):
+        car_list = Car.objects.all()
+        return {
+        "car_list": car_list
+        }
+    
